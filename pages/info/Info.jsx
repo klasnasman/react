4c3e7fe36a6eb59cdce8057fc7d7175bf4a7ae8b
klasnasman <klas.nasman@gmail.com>
2023-03-13 09:04:34 +0100

    info/wedding img > public

--- pages/info/Info.jsx
+++ pages/info/Info.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Info = () => {
     return (
         <div className='info__grid'>
-            <section className='info__section'><img src='/src/assets/img/info.png' />
+            <section className='info__section'><img src='/img/info.png' />
                 <p>Fotografering för mig är att bevara minnen.<br />Ett sätt att minnas människor vi mött och platser vi besökt.
                     <br />
                     Foton hjälper oss att göra precis det - <br />
