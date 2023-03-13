c2a6bbd594e56468deefb79a01a3f18fca2ca5f0
klasnasman <klas.nasman@gmail.com>
2023-03-13 10:59:10 +0100

    new footer info

---
+++ pages/components/FooterInfo.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const FooterInfo = () => {
+    return (
+        <footer>
+            <div className="f-logo">
+                <a href="#top">Info</a>
+            </div>
+            <div className="f-menu">
+                <ul>
+                    <li><a href="/portrait/">Portrait</a></li>
+                    <li><a href="/personal/">Personal</a></li>
+                    <li><a href="/wedding/">Wedding</a></li>
+                </ul>
+            </div>
+            <div className="f-contact">
+                <ul>
+                    <p>+46 70 549 64 74</p>
+                    <p>hello@klasnasman.se</p>
+                    <p>Stockholm, Sweden</p>
+                </ul>
+            </div>
+            <div className="f-follow">
+                <ul>
+                    <li><a href="#">Instagram</a></li>
+                    <li><a href="#">Twitter</a></li>
+                    <li><a href="#">LinkedIn</a></li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
+
+export default FooterInfo
--- pages/info/Info.jsx
+++ pages/info/Info.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import FooterInfo from '../components/FooterInfo'
 import Header from '../components/Header'
 import Transitions from '../Transitions'
 
@@ -18,6 +19,7 @@ const Info = () => {
                     </p>
                 </section>
             </div>
+            <FooterInfo />
         </Transitions>
     )
 }
