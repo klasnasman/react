62bbfa9580fef5566942692ef094712a04b82822
Klas Näsman <109417650+klasnasman@users.noreply.github.com>
2023-03-21 12:31:01 +0100

    Update Hero.jsx

--- pages/components/Hero.jsx
+++ pages/components/Hero.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Hero = () => {
     return (
         <section className="hero">
-            <span className="hero__heading">Technology made Beautiful.<br />
-                A future-focused digital studio passionate about bringing clarity to the complex.</span>
+            <span className="hero__heading">Lorem Ipsum.<br />
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac hendrerit nulla. Fusce euismod ante.</span>
         </section>
     )
 }
