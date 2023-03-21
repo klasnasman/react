614daaa369a386863d1a6461c0130be2ba87c23f
Klas Näsman <109417650+klasnasman@users.noreply.github.com>
2023-03-21 08:41:31 +0100

    Update Header.jsx

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -2,28 +2,17 @@ import React from 'react'
 import MobileMenu from './MobileMenu';
 import { useEffect } from 'react';
 
-const Header = () => {
+const Header() {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleClass = () => {
-        const menuMobile = document.getElementById('mobile__menu');
-        const menuBtn = document.getElementById('header__button');
-        menuMobile.classList.toggle("open");
-        menuBtn.textContent = menuBtn.textContent == 'Menu' ? 'Close' : 'Menu';
-    };
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-    useEffect(() => {
-        window.addEventListener('resize', () => {
-            const menuMobile = document.getElementById('mobile__menu');
-            const menuBtn = document.getElementById('header__button');
-            if (window.innerWidth > 950) {
-                menuMobile.classList.remove('open');
-                menuBtn.textContent = 'Menu';
-            }
-        });
-    });
+  const buttonText = isOpen ? 'Close' : 'Menu';
 
-    return (
-        <header className="header">
+  return (
+      <header className="header">
             <nav className="header__nav">
                 <div className="header__logo">
                     <a href="/">Klas Näsman</a>
@@ -34,9 +23,9 @@ const Header = () => {
                     <li><a href="/wedding">Wedding</a></li>
                     <li><a href="/info">Info</a></li>
                 </ul>
-                <button id="header__button" onClick={toggleClass}>Menu</button>
+                <button id="header__button" onClick={toggleMenu}>{buttonText}</button>
             </nav>
-            <MobileMenu />
+      <MobileMenu isOpen={isOpen} />
         </header>
 
 
