32af1701a127e30e172fc0418df02479599f93c3
Klas Näsman <109417650+klasnasman@users.noreply.github.com>
2023-03-22 06:27:36 +0100

    Update MobileMenu.jsx

--- pages/components/MobileMenu.jsx
+++ pages/components/MobileMenu.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 
 const MobileMenu = ({ isOpen }) => {
-  const menuHeight = 
-isOpen ? '25%' : '-100vh';
+  const menuHeight = isOpen ? '25%' : '-100vh';
 
   return (
-
   <div className="mobile__menu" style={{ top: menuHeight }}>
-
             <ul className='mobile__ul'>
                 <li><a href='/people'>People</a></li>
                 <li><a href='/places'>Places</a></li>
